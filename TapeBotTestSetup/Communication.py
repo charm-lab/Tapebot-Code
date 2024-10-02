770ba271efdb4d078a93162aca1f8f8aa66dca85
Frances Raphael <francesraphael42@gmail.com>
2024-10-02 13:47:06 -0700

    My most recent changes to the code

--- TapeBotTestSetup/Communication.py
+++ TapeBotTestSetup/Communication.py
@@ -1,5 +1,6 @@
 # Reading and Saving Serial Data from an Arduino and a Force Sensor 
 # Written by: Frances Raphael fraphael@stanford.edu
+
 import serial
 import datetime
 import csv
@@ -13,9 +14,9 @@ from scipy import signal
 RUNTIME_LENGTH = 30
  # second
 PATH_DEST = "/home/frances/Tapebot-Code/TapeBotTestSetup"
-PORT_NAME1 = "/dev/ttyS7"
+PORT_NAME1 = "/dev/ttyS8"
 PORT_NAME2 = "/dev/ttyS10"
-BAUD_RATE = 115200
+BAUD_RATE = 9600
 # DIRECTORY
 fileName = str(datetime.datetime.now())[0:16]  # default name is date and time
 fileName = ((fileName.replace('/', '_')).replace(' ', '_')).replace(':', '-')
@@ -24,25 +25,25 @@ if not (os.path.exists(p)):
     os.makedirs(p)
     print("New directory created: %s" % fileName)
 
-# # Check the device connections for Windows or Linix
-# if sys.platform.startswith('win'):
-#     ports = ['COM%s' % (i + 1) for i in range(256)]
-# elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-#     # this excludes your current terminal "/dev/tty"
-#     ports = glob.glob('/dev/tty[A-Za-z]*')
-# elif sys.platform.startswith('darwin'):
-#     ports = glob.glob('/dev/tty.*')
-# else:
-#     raise EnvironmentError('Unsupported platform')
-# result = []
-# for port in ports:
-#     try:
-#         s = serial.Serial(port)
-#         s.close()
-#         result.append(port)
-#     except (OSError, serial.SerialException):
-#         pass
-# print(result)
+# Check the device connections for Windows or Linix
+if sys.platform.startswith('win'):
+    ports = ['COM%s' % (i + 1) for i in range(256)]
+elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+    # this excludes your current terminal "/dev/tty"
+    ports = glob.glob('/dev/tty[A-Za-z]*')
+elif sys.platform.startswith('darwin'):
+    ports = glob.glob('/dev/tty.*')
+else:
+    raise EnvironmentError('Unsupported platform')
+result = []
+for port in ports:
+    try:
+        s = serial.Serial(port)
+        s.close()
+        result.append(port)
+    except (OSError, serial.SerialException):
+        pass
+print(result)
 
 
 # Functions to run when setting up the threads
@@ -56,27 +57,27 @@ def arduinoClass():
     # print("Arduino On!")
 
 
-def forcesensorClass():
-    global force
-    force = serial.Serial(port=PORT_NAME2, baudrate=BAUD_RATE, timeout=.1)
-    # clear the old data
-    force.flushInput()
-    force.flushOutput()
-    # print(str(datetime.datetime.now()))
-    # print("Force On!")
+# def forcesensorClass():
+#     global force
+#     force = serial.Serial(port=PORT_NAME2, baudrate=BAUD_RATE, timeout=.1)
+#     # clear the old data
+#     force.flushInput()
+#     force.flushOutput()
+#     # print(str(datetime.datetime.now()))
+#     # print("Force On!")
 
 
 # Starting up the Threads
 thread_one = threading.Thread(target=arduinoClass, args=())
-thread_two = threading.Thread(target=forcesensorClass, args=())
+#thread_two = threading.Thread(target=forcesensorClass, args=())
 
 # Start the threads at the same time
 thread_one.start()
-thread_two.start()
+#thread_two.start()
 
 # Join the Threads together
 thread_one.join()
-thread_two.join()
+#thread_two.join()
 
 # print("Done setting up the threads!")
 
@@ -87,11 +88,13 @@ writer = csv.writer(f)
 endTime = datetime.datetime.now() + datetime.timedelta(seconds=RUNTIME_LENGTH)
 while datetime.datetime.now() < endTime:
     arduinoData = arduino.readline().decode("utf-8").strip().split()
-    forceData = force.readline().decode("utf-8").strip().split()
+    #forceData = force.readline().decode("utf-8").strip().split()
+    #print(arduinoData)
+    #print(arduino.readline().decode("utf-8").strip().split())
 
-    if len(arduinoData) >= 9 and len(forceData) >= 1:
-        print([arduinoData[2], arduinoData[6], arduinoData[9], forceData[0]])
-        # Write the data into separate rows
-        writer.writerow([arduinoData[2], arduinoData[6],
-                         arduinoData[9], forceData[0]])
+    if len(arduinoData) >= 12: # and len(forceData) >= 1:
+        print([arduinoData[2], arduinoData[5], arduinoData[9], arduinoData[12]])
+        #Write the data into separate rows
+        writer.writerow([arduinoData[2], arduinoData[5],
+                         arduinoData[9], arduinoData[12]])
 f.close()
